5900b99a91281e73ce1137d7f0270df2097e6b43
PhongNT <phongnt.it.s@gmail.com>
2019-06-15 17:20:20 +0700

    no message

--- src/main/java/com/example/demo/entity/Specialized.java
+++ src/main/java/com/example/demo/entity/Specialized.java
@@ -1,6 +1,6 @@
 package com.example.demo.entity;
 
-import com.fasterxml.jackson.annotation.JsonManagedReference;
+import com.fasterxml.jackson.annotation.*;
 import org.hibernate.annotations.LazyCollection;
 import org.hibernate.annotations.LazyCollectionOption;
 
