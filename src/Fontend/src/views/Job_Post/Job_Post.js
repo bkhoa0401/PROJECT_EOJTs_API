bbce17c25eb3bf25766d249a3ffa9231df7c49f8
nguyenvohung <hungnvpse61803@fpt.edu.vn>
2019-06-22 23:23:59 +0700

    Job_Post

--- src/Fontend/src/views/Job_Post/Job_Post.js
+++ src/Fontend/src/views/Job_Post/Job_Post.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
-import { Badge, Card, CardBody, CardHeader, Col, Pagination, Row, Table, ButtonDropdown, DropdownItem, DropdownMenu, DropdownToggle } from 'reactstrap';
-import { Button } from 'reactstrap';
+import { Input, Button, Badge, Card, CardBody, CardHeader, Col, Pagination, Row, Table, ButtonDropdown, DropdownItem, DropdownMenu, DropdownToggle } from 'reactstrap';
 import ApiServices from '../../service/api-service';
 import { ToastContainer } from 'react-toastify';
 import orderBy from "lodash/orderBy";
@@ -24,16 +23,42 @@ class Job_Post extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      arrayJob: ['Kỹ thuật phần mềm', 'Kinh tế', 'Đồ hoạ', 'Ngôn ngữ']
-    };
+      arrayJob: ['Kỹ thuật phần mềm', 'Kinh tế', 'Đồ hoạ', 'Ngôn ngữ Anh', 'Ngôn ngữ Nhật'],
+      arrayJobPost: ['Kỹ thuật phần mềm', 'Kinh tế'],
+      arraySkillQuantity: [
+        ['Java: 30', 'C#: 20'],
+        ['QTKD: 10', 'KDQT: 10', 'QHQT: 1'],
+      ],
+      tmpArray:[],
+      selectedSpecialized:''
+    }
+    this.chooseNewSpecialized = this.chooseNewSpecialized.bind(this);
+    this.setNewSpecialized = this.setNewSpecialized.bind(this);
   }
 
-  handleDirect = (uri) => {
-    this.props.history.push(uri);
+  addRow = (sIndex) => {
+    // let tmpArray = this.state.arraySkillQuantity[sIndex].push('');
+    // this.setState({
+    //   tmpArray: [...this.state.tmpArray,''],
+    //   arraySkillQuantity: this.state.arraySkillQuantity,
+    // })
+  }
+
+  chooseNewSpecialized(event) {
+    this.setState({
+      selectedSpecialized: event.target.value,
+    })
+  }
+
+  setNewSpecialized(){
+    this.setState({
+      arrayJobPost: [...this.state.arrayJobPost, this.state.selectedSpecialized],
+      arraySkillQuantity: [...this.state.arraySkillQuantity, []],
+    })
   }
 
   render() {
-    const { arrayJob } = this.state;
+    const { arrayJob, arrayJobPost, arraySkillQuantity, isOpened } = this.state;
     return (
       <div className="animated fadeIn">
         <Row>
@@ -44,40 +69,53 @@ class Job_Post extends Component {
               </CardHeader>
               <CardBody>
                 <div style={{ textAlign: "center" }}>
-                  <Button 
-                    style={{ fontWeight: "bold", borderWidth: 0 }} color="primary"
-                    onClick={() => this.handleDirect(`Job_Post/Add_Job/`)}
+                  <Input type="select" name="specialized" id="specialized" style={{width:'500px', alignSelf:'center'}} onChange={event => this.chooseNewSpecialized(event)}>
+                    <option value="0">Ngành tuyển dụng</option>
+                    {arrayJob.map((specialized, index) =>
+                      <option value={specialized}>{specialized}</option>
+                    )}
+                  </Input>
+                  <Button
+                    style={{ fontWeight: "bold", borderColor: '#20a8d8', color: '#20a8d8', backgroundColor: 'white' }}
+                    onClick={this.setNewSpecialized}
                   >
-                    Thêm ngành tuyển dụng
+                    Thêm
                   </Button>
                   </div>
                 <Row>
-                    {arrayJob.map((job, index) =>
+                  {arrayJobPost.map((job, index1) =>
                     <>
                       <table style={table}>
-                        <thead 
-                          style={{ backgroundColor: '#20a8d8', color: 'white', textAlign: "center", fontWeight:"bold", height:"40px", fontSize:"16px" }}
+                        <thead
+                          style={{ backgroundColor: '#20a8d8', color: 'white', textAlign: "center", fontWeight: "bold", height: "40px", fontSize: "16px" }}
                           specialized={job}
                         >
                           {job}
                         </thead>
-                        <tbody style={{textAlign: "center", fontSize:"14px"}}>
-                          <tr>Java: 30</tr>
-                          <tr style={{ textAlign: "center", height:"40px" }}>
-                            <Button style={{ fontWeight: "bold"}} color="danger">Xoá</Button>
-                            <Button 
-                            style={{ fontWeight: "bold", borderColor:'#20a8d8', color: '#20a8d8', backgroundColor: 'white' }}
-                            onClick={() => this.handleDirect(`Job_Post/Update_Job/`)}
+                        <tbody style={{ textAlign: "center", fontSize: "14px" }}>
+                          {arraySkillQuantity[index1].map((skill, index2) =>
+                            <>
+                              <tr>
+                                <td>{index2}. {skill !== '' ? skill : <input></input>}</td>
+                              </tr>
+                            </>
+                          )
+                          }
+                          <tr style={{ textAlign: "center", height: "40px" }}>
+                            <Button style={{ fontWeight: "bold" }} color="danger">Xoá</Button>
+                            <Button
+                              style={{ fontWeight: "bold", borderColor: '#20a8d8', color: '#20a8d8', backgroundColor: 'white' }}
+                              onClick={this.addRow(index1)}
                             >
-                              Sửa
+                              Thêm
                             </Button>
                           </tr>
                           <tr></tr>
                         </tbody>
                       </table>
-                      </>
-                    )}
-                  
+                    </>
+                  )}
+
                 </Row>
                 <Pagination>
                   {/* <PaginationComponent pageNumber={pageNumber} handlePageNumber={this.handlePageNumber} handlePageNext={this.handlePageNext} handlePagePrevious={this.handlePagePrevious} currentPage={currentPage} /> */}
