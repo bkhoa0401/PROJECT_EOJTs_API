42f98a9b6fe97c3ccbb8f3fd18348352a465200a
khoa <bkhoa0401@gmail.com>
2019-07-22 00:21:40 +0700

    feat - APIs: accept by startup and headTraining and headMaster

--- src/main/java/com/example/demo/service/Business_ProposedService.java
+++ src/main/java/com/example/demo/service/Business_ProposedService.java
@@ -30,7 +30,6 @@ public class Business_ProposedService implements IBusiness_ProposedService {
         List<Business_Proposed> business_proposeds = iBusiness_proposedRepository.findAll();
 
         List<Business_Proposed> business_proposedsSemesterCurrent = new ArrayList<>();
-        ;
 
         for (int i = 0; i < business_proposeds.size(); i++) {
             Student student = business_proposeds.get(i).getStudent_proposed();
@@ -41,5 +40,41 @@ public class Business_ProposedService implements IBusiness_ProposedService {
         }
         return business_proposedsSemesterCurrent;
     }
+
+    @Override
+    public Business_Proposed findById(int id) {
+        return iBusiness_proposedRepository.findById(id);
+    }
+
+
+    @Override
+    public void updateStatusByStartUpRoom(int id, String comment, boolean status) {
+        Business_Proposed business_proposed = findById(id);
+        if (business_proposed != null) {
+            business_proposed.setCommentStartupRoom(comment);
+            business_proposed.setAcceptedByStartupRoom(true);
+            iBusiness_proposedRepository.save(business_proposed);
+        }
+    }
+
+    @Override
+    public void updateStatusByHeadOfTraining(int id, String comment, boolean status) {
+        Business_Proposed business_proposed = findById(id);
+        if (business_proposed != null) {
+            business_proposed.setCommentHeadOfTraining(comment);
+            business_proposed.setAcceptedByHeadOfTraining(true);
+            iBusiness_proposedRepository.save(business_proposed);
+        }
+    }
+
+    @Override
+    public void updateStatusByHeadMaster(int id, boolean status) {
+        Business_Proposed business_proposed = findById(id);
+        if (business_proposed != null) {
+            business_proposed.setAcceptedByHeadMaster(true);
+            iBusiness_proposedRepository.save(business_proposed);
+        }
+    }
+
 
 }
